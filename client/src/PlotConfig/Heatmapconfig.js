40e18fdedba065fa69e5f40efac3377748d40d3d
tinanemati <tinanemati.tina@gmail.com>
2023-11-21 11:05:11 -0800

    added options for plot config

---
+++ client/src/PlotConfig/Heatmapconfig.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+
+export default function Heatmapconfig({ configValue, updateConfigValue }) {
+  const options = ["Update zMin", "Update zMax", "Reset"];
+  console.log("this is the value:", configValue)
+
+  const handleChange = (event) => {
+    updateConfigValue(event.target.value);
+  };
+
+  return (
+    <FormControl sx={{ display: "flex"}}>
+      <FormLabel sx={{marginLeft: "10px"}}>Heatmap options:</FormLabel>
+      <RadioGroup
+        name="Heatmapconfig-radio-buttons-group"
+        value={configValue}
+        onChange={handleChange}
+        sx={{marginLeft: "10px"}}
+      >
+        {options.map((option, index) => (
+          <FormControlLabel
+            key={index}
+            value={option}
+            control={<Radio />}
+            label={option}
+          />
+        ))}
+      </RadioGroup>
+    </FormControl>
+  );
+}
