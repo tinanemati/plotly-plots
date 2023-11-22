90c02ec8050e4fbfe50c2dac7589508981bf3533
tinanemati <tinanemati.tina@gmail.com>
2023-11-21 16:35:02 -0800

    removed a hook that I was not using

--- client/src/PlotConfig/Heatmapconfig.js
+++ client/src/PlotConfig/Heatmapconfig.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
