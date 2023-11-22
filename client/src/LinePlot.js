ff416e5d518602cdec736c9c63df07dc71262be9
tinanemati <tinanemati.tina@gmail.com>
2023-11-21 16:58:26 -0800

    added config options for the line plot

--- client/src/LinePlot.js
+++ client/src/LinePlot.js
@@ -1,33 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Plot from "react-plotly.js";
+import Lineplotconfig from "./PlotConfig/Lineplotconfig";
 
-export default function LinePlot({xData, yData}) {
+export default function LinePlot({ xData, yData }) {
+  const [configValue, setConfigValue] = useState("");
+  const updateConfigValue = (newValue) => {
+    setConfigValue(newValue);
+  };
+  useEffect(() => {
+    if (configValue === "Integration") {
+      console.log("this is where we have to apply the integration logic")
+    } else if (configValue === "Baseline") {
+        console.log("this is where we have to apply the baseline logic")
+    } else if (configValue === "Reset") {
+        console.log("this is where we will apply the reset logic")
+    }
+  }, [configValue]);
+
+  const scrollZoom = configValue === "Standard" ? true : false;
+  const dragMode = configValue === "Standard" ?  "pan" : false;
 
   return (
-    <Plot
-      data={[
-        {
-          x: xData,
-          y: yData,
-          type: "scatter",
-          mode: "lines+markers",
-          marker: { color: "#6ECEB2" },
-        },
-      ]}
-      layout={{
-        width: 950,
-        height: 570,
-        title: "Extracted Ion Chromatogram",
-        xaxis: {
-          title: "Retention Time (Minute)",
-        },
-        yaxis: {
-          title: "Ion Count",
-        //   fixedrange: true,
-        },
-        dragmode: "pan", // Enable panning
+    <div
+      className="lineplot-style"
+      style={{
+        display: "flex",
+        alignItems: "center",
       }}
-      config={{ scrollZoom: true, displaylogo: false, displayModeBar: false  }} // Enable scroll zoom
-    />
+    >
+      <Lineplotconfig
+        configValue={configValue}
+        updateConfigValue={updateConfigValue}
+      />
+      <Plot
+        data={[
+          {
+            x: xData,
+            y: yData,
+            type: "scatter",
+            mode: "lines+markers",
+            marker: { color: "#6ECEB2" },
+          },
+        ]}
+        layout={{
+          width: 950,
+          height: 570,
+          title: "Extracted Ion Chromatogram",
+          xaxis: {
+            title: "Retention Time (Minute)",
+          },
+          yaxis: {
+            title: "Ion Count",
+          },
+          dragmode: dragMode
+        }}
+        config={{ scrollZoom: scrollZoom, displaylogo: false, displayModeBar: false }}
+      />
+    </div>
   );
 }
---
+++ client/src/PlotConfig/Lineplotconfig.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+
+export default function Lineplotconfig({ configValue, updateConfigValue }) {
+  const options = ["Standard","Integration", "Baseline", "Reset"];
+  //console.log("this is the value:", configValue)
+
+  const handleChange = (event) => {
+    updateConfigValue(event.target.value);
+  };
+
+  return (
+    <FormControl sx={{ display: "flex"}}>
+      <FormLabel sx={{marginLeft: "10px"}}>LinePlot options:</FormLabel>
+      <RadioGroup
+        name="Lineplotconfig-radio-buttons-group"
+        value={configValue}
+        onChange={handleChange}
+        sx={{marginLeft: "10px"}}
+      >
+        {options.map((option, index) => (
+          <FormControlLabel
+            key={index}
+            value={option}
+            control={<Radio />}
+            label={option}
+          />
+        ))}
+      </RadioGroup>
+    </FormControl>
+  );
+}
