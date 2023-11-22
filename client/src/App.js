f24ed2c81d6ac2707ca4639e2a1e854e12abb1fc
tinanemati <tinanemati.tina@gmail.com>
2023-11-21 17:10:37 -0800

    cleaned the app

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,6 @@ import LinePlot from "./components/InteractivePlots/LinePlot";
 export default function App() {
   const [xData, setXdata] = useState([]);
   const [yData, setYdata] = useState([]);
-  // const [yData1, setY1data] = useState([]);
-  // const [zData, setZdata] = useState([]);
 
   const updatexData = (newData) => {
     setXdata(newData);
@@ -15,12 +13,6 @@ export default function App() {
   const updateyData = (newData) => {
     setYdata(newData);
   };
-  // const updatey1Data = (newData) => {
-  //   setY1data(newData);
-  // };
-  // const updatezData = (newData) => {
-  //   setZdata(newData);
-  // };
 
   /* Testing
   console.log("this is my xData for line plotL: ", xData)
@@ -31,15 +23,8 @@ export default function App() {
     <div>
       <h1>Test Heatmap Components</h1>
 
-      <Heatmap
-        updatexData={updatexData}
-        updateyData={updateyData}
-        // updatezData={updatezData}
-        // updatey1Data={updatey1Data}
-      />
+      <Heatmap updatexData={updatexData} updateyData={updateyData} />
       <LinePlot xData={xData} yData={yData} />
-
-      {/* <Histogram zData={zData} /> */}
     </div>
   );
 }
