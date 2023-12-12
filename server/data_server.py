bc7cdf2ddbfcc160eaaee01c04d3a8a7d308ef30
tinanemati <tinanemati.tina@gmail.com>
2023-12-11 16:11:01 -0800

    added the genereic baseline support functionality to the server

--- server/data_server.py
+++ server/data_server.py
@@ -4,23 +4,24 @@ from json import loads
 import rainbow as rb
 import pandas as pd
 from pandas import DataFrame
+import numpy as np
 from scipy.integrate import simpson
 
 # Create the app instance
 app = Flask(__name__)
 CORS(app)
 
-defaultFilename = "../assests/data/001-D3F-A1-5068080-0015-4.D"
+defaultFilename = "/Users/nemati/Documents/Trial-Data/5087258-0018 2023-07-11-SecondTrialData/5087258-0018_Data/018-D2F-B6-5087258-0018.D"
 # Extract the file from the directory
 datadir = rb.read(defaultFilename)
 datafile = datadir.get_file("MSD1.MS")
-data = datafile.data.T
+well_data = datafile.data.T
 
 # Well data information that uses the path to the well we are looking for
 @app.route('/well')
 def well():
     # Extract the requested masses from the file
-    df = DataFrame(data, columns=datafile.xlabels.tolist(),
+    df = DataFrame(well_data, columns=datafile.xlabels.tolist(),
                    index=datafile.ylabels.tolist())
     df = df.query("260 < index < 340")
 
@@ -34,6 +35,23 @@ def well():
     }
 
     return jsonify(wellData)
+
+# Route that gives the baseline for the extracted (m/z)
+@app.route('/baseline', methods=['POST'])
+def baseline():
+    try:
+        # Retrieve data from the POST request
+        data = request.get_json()
+        #print(data)
+        yData = data.get("yData")
+        # create the most generic baseline
+        baseline = [min(yData)] * len(yData)
+
+        response = {"baseline": baseline}
+        return jsonify(response), 200
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 # Route that gives the path and channel that we need to process
 @app.route('/area', methods=['POST'])
