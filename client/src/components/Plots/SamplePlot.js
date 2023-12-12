e2681e1ec6c5e50b2a6dec5d5cee4afafeea70a9
tinanemati <tinanemati.tina@gmail.com>
2023-12-12 12:55:28 -0800

    added a feature in simple plot to change color when clicked

--- client/src/components/Plots/SamplePlot.js
+++ client/src/components/Plots/SamplePlot.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Plot from "react-plotly.js";
 
 export default function SamplePlot() {
+  const [pointClicked, setPointClicked] = useState([]);
   const arrayX = [1, 2, 3];
   const arrayY = [2, 6, 3];
-
+  const markerColors = arrayX.map((_, index) =>
+    pointClicked.includes(index) ? "green" : "red"
+  );
+  const handleClick = (data) => {
+    const clickedPointIndex = data.points[0].pointIndex;
+    const updatedClickedPoints = pointClicked.includes(clickedPointIndex)
+        ? pointClicked.filter((index) => index !== clickedPointIndex)
+        : [...pointClicked, clickedPointIndex];
+    setPointClicked(updatedClickedPoints)
+    console.log("onClick", data.points[0], "and this is my cliked points:", updatedClickedPoints);
+  };
   return (
     <Plot
       data={[
@@ -13,11 +24,13 @@ export default function SamplePlot() {
           y: arrayY,
           type: "scatter",
           mode: "lines+markers",
-          marker: { color: "red" },
+          marker: {
+            color: markerColors,
+          },
         },
-        { type: "bar", x: arrayX, y: arrayY },
       ]}
       layout={{ width: 320, height: 240, title: "A Fancy Plot" }}
+      onClick={handleClick}
     />
   );
 }
