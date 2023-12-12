0807bb89cfa1e5db9dde123d92d4a83391baf8b2
tinanemati <tinanemati.tina@gmail.com>
2023-12-12 14:26:56 -0800

    created end point to calculte baseline correction

--- server/data_server.py
+++ server/data_server.py
@@ -7,6 +7,7 @@ from pandas import DataFrame
 import numpy as np
 from scipy.integrate import simpson
 from scipy.integrate import trapezoid
+from scipy.interpolate import UnivariateSpline
 
 # Create the app instance
 app = Flask(__name__)
@@ -19,6 +20,8 @@ datafile = datadir.get_file("MSD1.MS")
 well_data = datafile.data.T
 
 # Well data information that uses the path to the well we are looking for
+
+
 @app.route('/well')
 def well():
     # Extract the requested masses from the file
@@ -38,16 +41,18 @@ def well():
     return jsonify(wellData)
 
 # Route that gives the baseline for the extracted (m/z)
+
+
 @app.route('/baseline', methods=['POST'])
 def baseline():
     try:
         # Retrieve data from the POST request
         data = request.get_json()
-        #print(data)
+        # print(data)
         yData = data.get("yData")
         # create the most generic baseline
         baseline = [min(yData)] * len(yData)
-        # update the raw data according to baseline 
+        # update the raw data according to baseline
         newYdata = [y - b for y, b in zip(yData, baseline)]
         response = {"baseline": baseline, "newYdata": newYdata}
         return jsonify(response), 200
@@ -56,6 +61,8 @@ def baseline():
         return jsonify({'error': str(e)})
 
 # Route that gives the path and channel that we need to process
+
+
 @app.route('/area', methods=['POST'])
 def area():
     try:
@@ -65,13 +72,13 @@ def area():
         xData = data.get("xData")
         range = data.get("range")
         baseline = data.get("baseline")
-        # update the raw data according to baseline 
+        # update the raw data according to baseline
         newYdata = [y - b for y, b in zip(yData, baseline)]
-        # slice the x and y data axis given the left and right side keys 
+        # slice the x and y data axis given the left and right side keys
         leftside = range['leftside']
         rightside = range['rightside']
 
-        xDataRange = xData[leftside:rightside]  
+        xDataRange = xData[leftside:rightside]
         yDataRange = newYdata[leftside:rightside]
         # calulate the area given the updated yData
         area = trapezoid(
@@ -81,6 +88,50 @@ def area():
         response = {"area": area}
         return jsonify(response), 200
 
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+# Route that will apply baseline correction and re-calculate area
+
+
+@app.route('/baselineCorrection', methods=['POST'])
+def baselineCorrection():
+    try:
+        # Retreive data from the POST request
+        data = request.get_json()
+        x_data = data.get("xData")
+        y_data = data.get("yData")
+        baseline_time_ranges = data.get("baselineTimeRange")
+        region_time = data.get("regionTime")
+        # update the arrays to numpy so operation can be easier
+        x_np = np.array(x_data)
+        y_np = np.array(y_data)
+        # perform baseline correction here
+        noise_mask = np.zeros_like(x_np, dtype=bool)
+        for noise_region in baseline_time_ranges:
+            x = x_np
+            noise_start, noise_end = noise_region
+            noise_mask |= (x >= noise_start) & (x <= noise_end)
+
+            noise_x = x[noise_mask]
+            noise = y_data[noise_mask]
+            spline = UnivariateSpline(noise_x, noise, k=1, s=1)
+            baseline = spline(x)
+
+        # update the raw data with the baseline
+        y_np = y_np - baseline
+
+        # extract the time region
+        min_time, max_time = region_time.min_time, region_time.max_time
+        time_indices = [i for i, time in enumerate(
+            x_np) if min_time <= time <= max_time]
+        # perform slicing to calculate area
+        y_np = y_np[time_indices]
+        selected_times = x_np[time_indices]
+        # calculate area by trapezoidal rule
+        area = np.trapz(y_np, selected_times)
+        response = {"area": area}
+        return jsonify(response), 200
     except Exception as e:
         return jsonify({'error': str(e)})
 
