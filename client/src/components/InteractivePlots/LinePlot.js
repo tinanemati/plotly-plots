ee006292bfa5b96d51ec4475a9a17c40b849f772
tinanemati <tinanemati.tina@gmail.com>
2023-12-12 12:56:57 -0800

    removed un-used code

--- client/src/components/InteractivePlots/LinePlot.js
+++ client/src/components/InteractivePlots/LinePlot.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import Plot from "react-plotly.js";
 import RegionTable from "../plotAg-grid/RegionTable";
 import Lineplotconfig from "../PlotConfig/Lineplotconfig";
-import { colors } from "@mui/material";
 
 export default function LinePlot({
   xData,
